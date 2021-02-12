31f07d726400d6ffb062649f8e685a1a1dfd109c
Rylan Meilutis <meilutisrt@gmail.com>
2021-02-12 08:49:50 -0500

    Line follower is done

--- Cims-Robot.c
+++ Cims-Robot.c
@@ -1,37 +1,42 @@
-#pragma config(Sensor, port1,  rightsensor,        sensorVexIQ_ColorGrayscale)
-#pragma config(Sensor, port2,  leftsensor,        sensorVexIQ_ColorGrayscale)
+#pragma config(Sensor, in1,    lineFollowerRIGHT,   sensorLineFollower)
+#pragma config(Sensor, in2,    lineFollowerCENTER,  sensorLineFollower)
+#pragma config(Sensor, in3,    lineFollowerLEFT,    sensorLineFollower)
 #pragma config(Motor,  motor1,          leftMotor,     tmotorNone, openLoop, driveLeft)
 #pragma config(Motor,  motor2,          rightMotor,    tmotorNone, openLoop, driveRight)
 
 task main()
 {
-	int cValue = 2048;//value for if the colorsensor is on the line or not
+	int threshold = 2048;//value for if the colorsensor is on the line or not
 	int timer = 0;//timer for timingout the motors spinning
 	int modeSelector = 0;//0 for line follow one foe teliop
 
 	while(modeSelector == 0){
-			if(SensorValue(rightsensor) > cValue && SensorValue(leftsensor) > cValue){
-				motor[leftMotor] = 50;
-				motor[rightMotor] = 50;
-			}
-			else if(SensorValue(rightsensor) > cValue && SensorValue(leftsensor) < cValue){
-				motor[leftMotor] = 50;
-				motor[rightMotor] = 40;
-			}
-			else if(SensorValue(rightsensor) < cValue && SensorValue(leftsensor) > cValue){
-				motor[leftMotor] = 40;
-				motor[rightMotor] = 50;
-			}
-			else if(SensorValue(rightsensor) < cValue && SensorValue(leftsensor) < cValue && timer <= 200){
-				motor[leftMotor] = -50;
-				motor[rightMotor] = -50;
-				timer++;
-			}
+			 if(SensorValue(lineFollowerRIGHT) > threshold)
+    {
+      // counter-steer right:
+      motor[leftMotor]  = 63;
+      motor[rightMotor] = 0;
+    }
+    // CENTER sensor sees dark:
+    if(SensorValue(lineFollowerCENTER) > threshold)
+    {
+      // go straight
+      motor[leftMotor]  = 63;
+      motor[rightMotor] = 63;
+    }
+    // LEFT sensor sees dark:
+    if(SensorValue(lineFollowerLEFT) > threshold)
+    {
+      // counter-steer left:
+      motor[leftMotor]  = 0;
+      motor[rightMotor] = 63;
+    }
+
 		else{
 			motor[leftMotor]= 0 ;
 				motor[rightMotor] = 0;
 		}
-	}
+
 	while(modeSelector == 1){
 			// set left side motors
 			motor[leftMotor] = (vexRT[Ch3] + vexRT[Ch4]);
@@ -40,4 +45,5 @@ task main()
 			motor[rightMotor] = (vexRT[Ch3] - vexRT[Ch4]);
 
 		}
+}
 }
