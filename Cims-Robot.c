5fd36b97af55a9677c84163a9c1671df1e8ccfc1
Rylan Meilutis <meilutisrt@gmail.com>
2021-03-12 13:58:25 -0500

    formatted and commented

--- Cims-Robot.c
+++ Cims-Robot.c
@@ -24,62 +24,60 @@ task main()
 	 wait(0.2);
 	 }
 
-		if(modeSelector == 0){
-			followLine(threshold, timer);
+	if(modeSelector == 0){
+		followLine(threshold, timer);
 		}
-		else{
+	else{
 		control();
 		}
 	}
-
 }
 
 void followLine(int threshold, int timer){
 	if(timer >= 200 && SensorValue(lineFollowerLEFT) > threshold && SensorValue(lineFollowerCENTER) > threshold && SensorValue(lineFollowerRIGHT) > threshold)
     {
     	timer++;
-      // counter-steer left:
       motor[leftMotor]  = 0;
       motor[rightMotor] = 0;
     }
-    else{
-	if(SensorValue(lineFollowerCENTER) > threshold)
-    {
-      // go straight
-      motor[leftMotor]  = 63;
-      motor[rightMotor] = 63;
-    }
-    // CENTER sensor sees dark:
-    else if(SensorValue(lineFollowerRIGHT) > threshold)
-    {
-    	timer = 0;
-      // counter-steer right:
-      motor[leftMotor]  = 63;
-      motor[rightMotor] = -63;
-    }
-    // LEFT sensor sees dark:
-    else if(SensorValue(lineFollowerLEFT) > threshold)
-    {
-      // counter-steer left:
-      motor[leftMotor]  = -63;
-      motor[rightMotor] = 63;
-    }
+   else{
+	   // Counter-steer left
+			if(SensorValue(lineFollowerCENTER) > threshold)
+	    {
+	      // go straight
+	      motor[leftMotor]  = 63;
+	      motor[rightMotor] = 63;
+	    }
+	    else if(SensorValue(lineFollowerRIGHT) > threshold)
+	    {
+	    	timer = 0;
+	      // counter-steer right:
+	      motor[leftMotor]  = 63;
+	      motor[rightMotor] = -63;
+	    }
+	    // LEFT sensor sees dark:
+	    else if(SensorValue(lineFollowerLEFT) > threshold)
+	    {
+	      // counter-steer left:
+	      motor[leftMotor]  = -63;
+	      motor[rightMotor] = 63;
+	    }
 
-		else{
-			motor[leftMotor]= 0 ;
-				motor[rightMotor] = 0;
+			else{
+				motor[leftMotor]= 10 ;
+					motor[rightMotor] = 10;
+			}
 		}
 	}
-}
-void control(){
-	if(vexRT[Ch3] > 0.02 || vexRT[Ch4] > 0.02){
-	motor[leftMotor] = (vexRT[Ch3] + vexRT[Ch4]);
+	void control(){
+		if(vexRT[Ch3] > 0.02 || vexRT[Ch4] > 0.02){
+		motor[leftMotor] = (vexRT[Ch3] + vexRT[Ch4]);
 
-			// set right side motors
-		motor[rightMotor] = (vexRT[Ch3] - vexRT[Ch4]);
-}
-else{
-	motor[leftMotor]= 0 ;
-	motor[rightMotor] = 0;
-}
+				// set right side motors
+			motor[rightMotor] = (vexRT[Ch3] - vexRT[Ch4]);
+	}
+	else{
+		motor[leftMotor]= 0 ;
+		motor[rightMotor] = 0;
+		}
 }
