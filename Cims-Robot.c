f7bf56af28e323cc43b1fb0b7135841ef4fbd969
Rylan Meilutis <meilutisrt@gmail.com>
2021-03-12 14:06:41 -0500

    fixed formatting

--- Cims-Robot.c
+++ Cims-Robot.c
@@ -15,69 +15,69 @@ task main()
 	int modeSelector = 0;
 	int timer = 0;
 	while(true){
-	if(SensorValue[touch] == 1 && modeSelector == 0){
-	 modeSelector=1;
-	 wait(0.2);
-	}
-	else if(SensorValue[touch] == 1 && modeSelector == 1){
-	 modeSelector=0;
-	 wait(0.2);
-	 }
-
-	if(modeSelector == 0){
-		followLine(threshold, timer);
+		if(SensorValue[touch] == 1 && modeSelector == 0){
+			modeSelector=1;
+			wait(0.2);
 		}
-	else{
-		control();
+		else if(SensorValue[touch] == 1 && modeSelector == 1){
+			modeSelector=0;
+			wait(0.2);
+		}
+
+		if(modeSelector == 0){
+			followLine(threshold, timer);
+		}
+		else{
+			control();
 		}
 	}
 }
 
 void followLine(int threshold, int timer){
 	if(timer >= 200 && SensorValue(lineFollowerLEFT) > threshold && SensorValue(lineFollowerCENTER) > threshold && SensorValue(lineFollowerRIGHT) > threshold)
-    {
-    	timer++;
-      motor[leftMotor]  = 0;
-      motor[rightMotor] = 0;
-    }
-   else{
-	   // Counter-steer left
-			if(SensorValue(lineFollowerCENTER) > threshold)
-	    {
-	      // go straight
-	      motor[leftMotor]  = 63;
-	      motor[rightMotor] = 63;
-	    }
-	    else if(SensorValue(lineFollowerRIGHT) > threshold)
-	    {
-	    	timer = 0;
-	      // counter-steer right:
-	      motor[leftMotor]  = 63;
-	      motor[rightMotor] = -63;
-	    }
-	    // LEFT sensor sees dark:
-	    else if(SensorValue(lineFollowerLEFT) > threshold)
-	    {
-	      // counter-steer left:
-	      motor[leftMotor]  = -63;
-	      motor[rightMotor] = 63;
-	    }
+	{
+		timer++;
+		motor[leftMotor]  = 0;
+		motor[rightMotor] = 0;
+	}
+	else{
+		// Counter-steer left
+		if(SensorValue(lineFollowerCENTER) > threshold)
+		{
+			// go straight
+			motor[leftMotor]  = 63;
+			motor[rightMotor] = 63;
+		}
+		else if(SensorValue(lineFollowerRIGHT) > threshold)
+		{
+			timer = 0;
+			// counter-steer right:
+			motor[leftMotor]  = 63;
+			motor[rightMotor] = -63;
+		}
+		// LEFT sensor sees dark:
+		else if(SensorValue(lineFollowerLEFT) > threshold)
+		{
+			// counter-steer left:
+			motor[leftMotor]  = -63;
+			motor[rightMotor] = 63;
+		}
 
-			else{
-				motor[leftMotor]= 10 ;
-					motor[rightMotor] = 10;
-			}
+		else{
+			motor[leftMotor]= 10 ;
+			motor[rightMotor] = 10;
 		}
 	}
-	void control(){
-		if(vexRT[Ch3] > 0.02 || vexRT[Ch4] > 0.02){
+}
+void control(){
+	if(vexRT[Ch3] > 0.02 || vexRT[Ch4] > 0.02){
 		motor[leftMotor] = (vexRT[Ch3] + vexRT[Ch4]);
 
-				// set right side motors
-			motor[rightMotor] = (vexRT[Ch3] - vexRT[Ch4]);
+		// set right side motors
+		motor[rightMotor] = (vexRT[Ch3] - vexRT[Ch4]);
 	}
 	else{
 		motor[leftMotor]= 0 ;
 		motor[rightMotor] = 0;
-		}
+	}
 }
